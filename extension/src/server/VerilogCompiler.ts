fbd57976f26928422f1893c04ddd7936bcea4bce
Nicolas CLERC <Nicolas CLERC>
2017-08-16 17:27:04 +0200

    adding parsing of multiline error message

--- extension/src/server/VerilogCompiler.ts
+++ extension/src/server/VerilogCompiler.ts
@@ -44,9 +44,9 @@ export namespace Compilers {
                         console.log(stderr);
                         console.log("## ----------------------")
                         // splitting stderr and removing filename
-                        let lines :string[] = stderr.split("\n").map( l=> l.replace(fileToCompile,""));
+                        let lines :string[] = stderr.split("\n"); //.map( l=> l.replace(fileToCompile,""));
                         console.log(lines.length + " line(s) after split");
-                        let diagnostics = this.GetDiagnostics(lines);
+                        let diagnostics = this.GetDiagnostics(fileToCompile, lines);
                         sendDiag(diagnostics);
                     }
                     else{
@@ -58,9 +58,10 @@ export namespace Compilers {
                 });
         }
 
-        private GetDiagnostics(lines: string[]): Diagnostic[] {
+        private GetDiagnostics(filename:string, lines: string[]): Diagnostic[] {
             let diagnostics: Diagnostic[] = [];
             let lastLineNumber:number=1;
+            let lastDiagnostic: Diagnostic = null;
 
             for(let l of lines) {
                 if(l.trim()=="")
@@ -72,30 +73,37 @@ export namespace Compilers {
 
                 let message:string="";
                 let lineNumber:number=1;
-                // normally, error line looks like ':LINENUM: message'
-                if (l.startsWith(":")) {
-                    let pos = l.indexOf(":",1);
-                    lineNumber = Number(l.substring(1,pos))-1; // because vscode diagnostic line numbering start a 0
-                    message = l.substring(pos+1).trim();
-                    if (message.startsWith("error:")) { // clean message text
-                        message=message.substring(6);
-                    }
+                if (lastDiagnostic && !l.startsWith(filename)) { // line that does not start with the filename -> add to previous diag
+                    lastDiagnostic.message+= (" " + l);
                 }
                 else {
-                    message= l;
-                    lineNumber=lastLineNumber; // unable to detect line number -> use the last detected
-                    //diagseverity=DiagnosticSeverity.Warning; // but as warning
+                    let l2 = l.replace(filename,"");
+                    // normally, error line looks like ':LINENUM: message'
+                    if (l2.startsWith(":")) {
+                        let pos = l2.indexOf(":",1);
+                        lineNumber = Number(l2.substring(1,pos))-1; // because vscode diagnostic line numbering start a 0
+                        message = l2.substring(pos+1).trim();
+                        if (message.startsWith("error:")) { // clean message text
+                            message=message.substring(6);
+                        }
+                    }
+                    else {
+                        message= l2;
+                        lineNumber=lastLineNumber; // unable to detect line number -> use the last detected
+                        //diagseverity=DiagnosticSeverity.Warning; // but as warning
+                    }
+                    lastDiagnostic={
+                        severity: diagseverity,
+                        range: { 
+                            start: {line: lineNumber, character:0 },
+                            end: { line: lineNumber, character: Number.MAX_VALUE}
+                        },
+                        message: `${message}`,
+                        source:''
+                    };
+                    diagnostics.push(lastDiagnostic);
+                    lastLineNumber=lineNumber;
                 }
-                diagnostics.push({
-                    severity: diagseverity,
-                    range: { 
-                        start: {line: lineNumber, character:0 },
-                        end: { line: lineNumber, character: Number.MAX_VALUE}
-                    },
-                    message: `${message}`,
-                    source:''
-                });
-                lastLineNumber=lineNumber;
             };
             return diagnostics;
         }
